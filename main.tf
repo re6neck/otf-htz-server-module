08794691ddda99f50e2cdb93c2d282d4df4682f0
re6neck <1027377+re6neck@users.noreply.github.com>
2025-07-04 15:21:55 +0200

    Add cloud init

--- main.tf
+++ main.tf
@@ -1,11 +1,19 @@
+data "cloudinit_config" "user_data" {
+  gzip          = true
+  base64_encode = true
+
+  part {
+    filename     = "cloud-config.yml"
+    content_type = "text/cloud-config"
+    content      = yamlencode(var.cloud_init["config"])
+  }
+}
+
 resource "hcloud_server" "vm" {
     name        = var.server_name
     server_type = var.server_type
     location    = var.server_location
     image       = var.image
     ssh_keys    = var.ssh_keys
-}
-
-output "ipv4_address" {
-    value = hcloud_server.vm.ipv4_address
+    user_data   = data.cloudinit_config.user_data.rendered
 }
